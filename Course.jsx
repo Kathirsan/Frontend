c9ea0b738f97e35968ef2cdeb5c392ef66d745b0
Kathirsan <vivekanantharajakathirsan2003@gmail.com>
2026-03-20 04:10:55 +0530

    today stard coding

---
+++ Course.jsx
@@ -0,0 +1,20 @@
+
+
+function Course(props){
+
+   
+    return(
+        < div className ="card">
+        
+            <img src={props.image} alt="" />
+            <h3>{props.name}</h3>
+            <p>{props.price}</p>
+            <span>{props.rating}</span>
+        </div>
+    );
+
+}
+
+
+
+export default Course
---
+++ CourseList.jsx
@@ -0,0 +1,50 @@
+import Course from './Course';
+import html from './assets/HTML.jpg'
+import css from './assets/CSS.jpg'
+import js from './assets/JS.jpg'
+
+
+
+function CourseList(){
+    const courses =[{
+        id:1,
+         name:"HTML Full Course" ,
+         price:"$199",
+         image:html,
+          rating:0
+},
+    {id:2,
+        name :"CSS",
+        price :"$199",
+        image:css,
+        rating : 5
+    },
+    {id:3,
+        name :"JavaScript",
+        price:"$499",
+        image :js,
+        rating :4
+
+    },
+    {id:4,
+        name :"React",
+        price:"$99",
+        image :js,
+        rating :5
+    }
+    ]
+    const coursesList=courses.map(
+        (course,index) =>
+            <Course key={index} name={course.name}
+        image={course.image}
+        price={course.price} rating={course.rating}/>)
+
+    
+    return(
+      
+    <>
+    {coursesList}
+      </>
+    );
+}
+export default CourseList;
