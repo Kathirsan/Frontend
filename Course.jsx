950f4652984a07a3e05d41a222c5deffd9718685
Kathirsan <vivekanantharajakathirsan2003@gmail.com>
2026-03-21 04:59:13 +0530

    upload

--- Course.jsx
+++ Course.jsx
@@ -5,11 +5,12 @@ import { useState } from "react";
 function Course(props){
     //let purchased = false;
     const [purchased,setPurchased] = useState('false');
+    const [discount,setDiscount]= useState(props.price);
 
-
-    function BuyCourse(discount){
+    function BuyCourse(amt){
         console.log(props.name,"purchased with",discount,"%discount");
         setPurchased('true');
+        setDiscount(discount-amt);
         console.log(purchased);
     }
 
@@ -20,7 +21,7 @@ function Course(props){
             <img src={props.image} alt="" />
             <h3>{props.name}</h3>
             <p>{props.price}</p>
-            <button onClick={(event)=> BuyCourse[20]}>Buy Now</button>
+            <button onClick={(event)=> {BuyCourse[20]}}>Discount</button>
             <p>{purchased }</p>
         </div>
     );
