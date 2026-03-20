49b1ad2dad18bbd985f45e1b6905f16f54bc8927
Kathirsan <vivekanantharajakathirsan2003@gmail.com>
2026-03-20 17:16:42 +0530

    update

--- CourseList.jsx
+++ CourseList.jsx
@@ -36,7 +36,7 @@ function CourseList(){
 
     courses.sort((x,y) => y.price - x.price)
 
-    const vfmCourse = cou
+    const vfmCourse = courses.filter((course)=> course.price<200)
 
     const coursesList=courses.map(
         (course,index) =>
