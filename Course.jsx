3d0fc108cc5e99d975bbd09fe391b7405dd6579b
Kathirsan <vivekanantharajakathirsan2003@gmail.com>
2026-03-20 17:33:46 +0530

    update

--- Course.jsx
+++ Course.jsx
@@ -12,7 +12,7 @@ function Course(props){
             <img src={props.image} alt="" />
             <h3>{props.name}</h3>
             <p>{props.price}</p>
-            <button>Buy Now</button>
+            <button onClick={B}>Buy Now</button>
         </div>
     );
 
