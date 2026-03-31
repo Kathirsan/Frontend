2230d4dd7c2504eb562dfba24d96438c298e3e37
Kathirsan <vivekanantharajakathirsan2003@gmail.com>
2026-03-31 06:37:01 +0530

    add code

--- App.jsx
+++ App.jsx
@@ -8,18 +8,24 @@ import ProductCard from './ProductCard';
 import { useState } from 'react';
 
 function App() {
- const [username,setUserName]=useState("");
-const [password,setPassword]=useState("");
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
 
-  return(
+  return (
     <div>
       <h1>Hello{username}</h1>
       <p>Your password is {password}</p>
-      <form>
-        <input type="text" placeholder="Enter username" value={username} onChange={(e)=>setUserName(e.target.value)}/>
-        <input type="password"placeholder="Enter password" value={password}onChange={(e)=>setPassword(e.target.value)} />
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log("Username:", username);
+          console.log("Password:", password);
+        }}>
+
+        <input type="text" placeholder="Enter username" value={username} onChange={(e) => setUserName(e.target.value)} />
+        <input type="password" placeholder="Enter password" value={password} onChange={(e) => setPassword(e.target.value)} />
       </form>
-     
+
 
     </div>
   )
